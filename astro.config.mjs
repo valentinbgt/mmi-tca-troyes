7c9c2d24bd5c84080acbea17251d54392f3286a2
Thomas Castellengo <thomas.castellengo@gmail.com>
2025-08-24 11:33:09 +0200

    deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/mmi-tca-troyes",
+  redirects: {
+    "/": "/mmi-tca-troyes/s5",
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });

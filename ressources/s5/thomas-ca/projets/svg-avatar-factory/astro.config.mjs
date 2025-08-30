73aba3488eda5f7a517a2460cc99e4df1416079a
Thomas Castellengo <thomas.castellengo@gmail.com>
2025-08-30 06:03:41 +0200

    Workflow de déploiement continue sur GitHub Pages
    Déploiement du projet Astro avec un workflow GitHub Actions contenant
    les étanpes suivantes :
    - Utilisation de npm pour le build
    - Utilisation de git pour pousser le code sur la branche gh-pages
    
    Configuration de Astro pour la production :
    - base: /mmi-tca-troyes/s5/thomas-ca/projets/svg-avatar-factory/

--- ressources/s5/thomas-ca/projets/svg-avatar-factory/astro.config.mjs
+++ ressources/s5/thomas-ca/projets/svg-avatar-factory/astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
-  integrations: [react()]
+  base: "/mmi-tca-troyes/s5/thomas-ca/projets/svg-avatar-factory/",
+  integrations: [react()],
 });
--- ressources/s5/thomas-ca/projets/svg-avatar-factory/supabase/functions/generate-avatar/index.ts
+++ ressources/s5/thomas-ca/projets/svg-avatar-factory/supabase/functions/generate-avatar/index.ts
@@ -155,9 +155,7 @@ Deno.serve(async (req: Request) => {
   2. Make an HTTP request:
 
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/generate-avatar' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
     --data '{}'
-
 
 */
